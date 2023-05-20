979e192daefd33d4557d335cea5aea1d5b0362d6
truong <truongphatroy@gmail.com>
2023-05-21 02:19:37 +0900

    change hashRouter to up github

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import React from 'react';
+import React from "react";
 import {
-	BrowserRouter,
-	Routes,
-	Route,
+  // BrowserRouter,
+  HashRouter,
+  Routes,
+  Route,
 } from "react-router-dom";
 
-import Browse from './pages/browse/Browse';
-import Search from './pages/search/Search';
-
+import Browse from "./pages/browse/Browse";
+import Search from "./pages/search/Search";
 
 function App() {
-	return (
-		<BrowserRouter>
-		  <Routes>
-			<Route path="/" element={<Browse/>}/>
-			<Route path="/search" element={<Search/>}/>
-		  </Routes>
-		</BrowserRouter>
-	  );
+  return (
+    <HashRouter>
+      <Routes>
+        <Route path='/' element={<Browse />} />
+        <Route path='/search' element={<Search />} />
+      </Routes>
+    </HashRouter>
+  );
 }
 
 export default App;
