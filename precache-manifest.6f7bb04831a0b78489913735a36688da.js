a3e08120684550e1aba20fe0aa96a18b6bda3677
truong <truongphatroy@gmail.com>
2023-05-21 02:05:46 +0900

    Updates

---
+++ precache-manifest.6f7bb04831a0b78489913735a36688da.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c26b1cfaa03bf5b2e976fb302d71005",
+    "url": "/movie-app/index.html"
+  },
+  {
+    "revision": "5a9487a041bf13bae94f",
+    "url": "/movie-app/static/css/2.520857cf.chunk.css"
+  },
+  {
+    "revision": "50bc3ac7279dd9d53eca",
+    "url": "/movie-app/static/css/main.7f532649.chunk.css"
+  },
+  {
+    "revision": "5a9487a041bf13bae94f",
+    "url": "/movie-app/static/js/2.fce47a4e.chunk.js"
+  },
+  {
+    "revision": "0531c2f40b27b6b0cbcbbc69fb4ce33d",
+    "url": "/movie-app/static/js/2.fce47a4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50bc3ac7279dd9d53eca",
+    "url": "/movie-app/static/js/main.09cd4d93.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/movie-app/static/js/main.09cd4d93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "150ff09411aacc292ed2",
+    "url": "/movie-app/static/js/runtime-main.60882319.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/movie-app/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/movie-app/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/movie-app/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/movie-app/static/media/slick.f97e3bbf.svg"
+  }
+]);
